1fffde5d2da032e0c9017d46ce9f8516eeddda40
hadi-xlx <hadi@xylex.ai>
2024-06-26 18:47:55 +0200

    polishing

--- tests/selecting.rs
+++ tests/selecting.rs
@@ -1,21 +1,26 @@
+use scylla::QueryResult;
+use scylla::transport::errors::QueryError;
 
 use scylladb_rs::ScyllaClient;
 use scylladb_rs::query::utils::print_query_result;
+
 
 #[tokio::test]
 async fn selecting() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     
     let client: ScyllaClient = ScyllaClient::new(vec!["127.0.0.1"]).await?;
 
-    // Test is_null method
-    let select_query= client
+    let select_query: Result<QueryResult, QueryError> = client
         .query("test", "test2_table")
         .select(&["name", "age"])
         .eq("age", 48)
         .execute()
-        .await?;
+        .await;
 
-    print_query_result("Result", &select_query);
+    match &select_query {
+        Ok(query_result) => print_query_result("Result", query_result),
+        Err(e) => println!("Query failed: {:?}", e),
+    }
 
     Ok(())
 }
