d8ac5cb42b9f696ded69e893bf0e82366072372c
hadi-xlx <hadi@xylex.ai>
2024-06-29 17:29:00 +0200

    docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! # Scyla SDK for Rust
+//! # Scylla SDK for Rust
 //!
 //! ## Examples
 //! test
@@ -191,7 +191,145 @@
 //!     Ok(())
 //! }
 //! ```
-
+//!
+//! ### Create a materialized view
+//! ```rust
+//! async fn create_materialized_view() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+//!    
+//!     let client: ScyllaClient = ScyllaClient::new(vec!["127.0.0.1"]).await?;
+//!
+//!     client.create_mv(
+//!         "test_keyspace",
+//!         "test_mv",
+//!         "test_table",
+//!         "age = 22",
+//!         "id"
+//!     ).await?;
+//!
+//!     Ok(())
+//! }
+//! ```
+//!
+//! ### Drop a materialized view
+//! ```rust
+//! async fn drop_materialized_view() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+//!    
+//!     let client: ScyllaClient = ScyllaClient::new(vec!["127.0.0.1"]).await?;
+//!
+//!     // Drop the materialized view
+//!     client.drop_mv(
+//!         "test_keyspace",
+//!         "test_mv"
+//!     ).await?;
+//!
+//!     Ok(())
+//! }
+//! ```
+//!
+//! ### Select query 1
+//! ```rust
+//! async fn test_select_query_1() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+//!     let client: ScyllaClient = ScyllaClient::new(vec!["127.0.0.1"]).await?;
+//!
+//!     // Here we use ALLOW FILTERING because we did't provide the complete key values
+//!     let select_query_1: Result<QueryResult, QueryError> = client
+//!         .query("test_keyspace", "test_table")
+//!         .select(&["name"])
+//!         .eq("age", 33)
+//!         .allow_filtering() 
+//!         .execute()
+//!         .await;
+//!
+//!     match &select_query_1 {
+//!         Ok(query_result) => print_query_result("Query 1:", query_result),
+//!         Err(e) => println!("Query 1 failed: {:?}", e),
+//!     }
+//!
+//!     Ok(())
+//! }
+//! ```
+//!
+//! ### Select query 2
+//! ```rust
+//! async fn test_select_query_2() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+//!     let client: ScyllaClient = ScyllaClient::new(vec!["127.0.0.1"]).await?;
+//!
+//!     //here we use ALlow filtering because only EQ and IN relation are supported on partition keys
+//!     let select_query_2: Result<QueryResult, QueryError> = client
+//!         .query("test_keyspace", "test_table")
+//!         .select(&["name", "age"])
+//!         .eq("user_id", 2)
+//!         .between("age", 20, 50)
+//!         .like("name", "Jane%") // you may also do "%Jane%" or just "Jane"
+//!         .allow_filtering()
+//!         .execute()
+//!         .await;
+//!
+//!     match &select_query_2 {
+//!         Ok(query_result) => print_query_result("Query 2:", query_result),
+//!         Err(e) => println!("Query 2 failed: {:?}", e),
+//!     }
+//!
+//!     Ok(())
+//! }
+//! ```
+//!
+//! ### Select query 3
+//! ```rust
+//! async fn test_select_query_3() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+//!     let client: ScyllaClient = ScyllaClient::new(vec!["127.0.0.1"]).await?;
+//!
+//!     // here we don't use ALLOW FILTERING because we provided the complete key values
+//!     // and all operations are suppotred for the partition keys
+//!     let select_query_3: Result<QueryResult, QueryError> = client
+//!         .query("test_keyspace", "test_table")
+//!         .select(&["name"])
+//!         .eq("user_id", 2)
+//!         .in_list("age", &vec![28, 44, 40]) // the vec can also have strings
+//!         .execute()
+//!         .await;
+//!
+//!     match &select_query_3 {
+//!         Ok(query_result) => print_query_result("Query 3:", query_result),
+//!         Err(e) => println!("Query 3 failed: {:?}", e),
+//!     }
+//!
+//!     Ok(())
+//! }
+//! ```
+//!
+//! ### Updating
+//! ```rust
+//! async fn updating() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+//!    
+//!     let client: ScyllaClient = ScyllaClient::new(vec!["127.0.0.1"]).await?;
+//!
+//!     let json_body = json!(
+//!         {
+//!             "score": 66.0
+//!         }
+//!     );
+//!
+//!     let mut primary_keys: HashMap<&str, i32> = HashMap::new();
+//!     primary_keys.insert("age", 44);
+//!     primary_keys.insert("user_id", 2);
+//!
+//!     let update = client
+//!         .query("test_keyspace", "test_table")
+//!         .update(
+//!             primary_keys,
+//!             json_body
+//!         )
+//!         .await;
+//!
+//!     match &update {
+//!         Ok(query_result) => println!("Query Succecsfull: {:?}", query_result),
+//!         Err(e) => println!("Query 1 failed: {:?}", e),
+//!     }
+//!
+//!     Ok(())
+//! }
+//! ```
 
 use scylla::Session;
 
